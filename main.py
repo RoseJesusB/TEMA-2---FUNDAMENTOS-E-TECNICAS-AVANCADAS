2a2d69ad6835296aa74272156ede2c9715fa6019
RoseJesusB <rosilainejesus43@gmail.com>
2026-01-17 09:07:25 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Tema 2 - Fundamentos e Técnicas Avançadas")
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print(nome, "é maior de idade.")
+else:
+    print(nome, "é menor de idade.")
